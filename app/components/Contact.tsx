90552cdf8cd79cdb26377a4946a32d154833c6b6
XI <khchow@xandi.co.jp>
2026-04-25 20:18:13 +0600

    .env file fix.

--- app/components/Contact.tsx
+++ app/components/Contact.tsx
@@ -36,13 +36,10 @@ const Contact = ({ isDarkMode }: ContactProps) => {
 
     try {
       // EmailJS configuration
-      // const serviceId = process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID!;
-      // const templateId = process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID!;
-      // const publicKey = process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY!;
-      // Temporarily hardcode to test
-      const serviceId = "service_spwepts";
-      const templateId = "template_7jxxwyi";
-      const publicKey = "vHmAFoRfrLtp8g69F";
+      const serviceId = process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID!;
+      const templateId = process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID!;
+      const publicKey = process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY!;
+
 
       if (!serviceId || !templateId || !publicKey) {
         throw new Error("EmailJS configuration is missing");
